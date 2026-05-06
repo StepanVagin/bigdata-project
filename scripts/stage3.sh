b93ad1e467e895d9fde6b739df48042d0fde43ec
Stepan Vagin <step.way.a12@gmail.com>
2026-05-06 13:49:22 +0300

    update stage3

--- scripts/stage3.sh
+++ scripts/stage3.sh
@@ -2,12 +2,8 @@
 export HADOOP_CONF_DIR=/etc/hadoop/conf
 export YARN_CONF_DIR=/etc/hadoop/conf
 
-python3 -m venv .venv
+
 source .venv/bin/activate
-
-
-pip install --upgrade pip 
-pip install -r requirements.txt
 
 export PYSPARK_PYTHON="$(pwd)/.venv/bin/python3"          
 spark-submit --master yarn scripts/model.py
